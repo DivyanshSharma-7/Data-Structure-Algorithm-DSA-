fab8643fde3ff5102ecff1f7d1557f1bbf9b4fc1
Divyansh Sharma <ds3741777@gmail.com>
2026-06-03 14:23:02 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int divy =19;
+    char grade = 'a';
+    float PI = 3.14f;
+    bool issafe =true;
+    double price =100.78;
+    int newprice=(int)price;
+int value=grade;
+cout<<"Divyansh\n" << "Sharma" <<endl; // 1st output print
+cout<< sizeof(divy) << "\n"; //size of datatypes
+cout<< divy <<"\n"; //integer
+cout<< grade<<"\n"; // characer 
+cout<<PI<<endl; //float
+cout<< issafe <<endl;  // true =1 , false =0 means boolean
+cout<<value<<endl; // type conversion
+cout<<newprice<<endl; // type casting
+
+return 0;
+}
+
+
+
