eb505e04a2cc958092e15bcadb1fe44f0f413d1f
Divyansh Sharma <ds3741777@gmail.com>
2026-06-03 15:04:54 +0530

    do while file

---
+++ loops/dowhile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int sum=0,n=5;
+    do{
+      //  sum=sum+i;
+      cout<< i <<" ";
+        i++;
+    }while(i<=n);
+    // cout<< sum;
+   return 0; 
+}
