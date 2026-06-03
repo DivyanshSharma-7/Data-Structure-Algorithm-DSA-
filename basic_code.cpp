3f910e153bfc62e2ed574efdcec1530d931cebdc
Divyansh Sharma <ds3741777@gmail.com>
2026-06-03 14:56:12 +0530

    Add Basic code file

---
+++ basic_code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int sum;
+    cout<<"enter a : ";
+
+    cin>>a;
+
+    cout<<"enter b : ";
+    cin>>b;
+
+    sum=a+b;
+    cout<<"sum = " << sum <<"\n" ;
+    cout<<"sum ="<<(a+b)<<"\n";
+cout<<"difference ="<<(a-b)<<"\n";
+cout<<"product = "<<(a*b)<<"\n";
+cout<<"division = "<<(a/b)<<"\n";
+cout<<"modulo ="<<(a%b)<<"\n";
+
+    return 0;
+}
