07694740cf813bc83ba4b0d52ab9cf7569c1bdc1
Divyansh Sharma <ds3741777@gmail.com>
2026-06-04 13:51:16 +0530

    Add basics file

---
+++ functions/basics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//function definition .
+int printHello()
+{
+cout<<"hello\n";
+return 3;
+}
+int main(){
+    // function call or fuction invoke .
+   int value = printHello();
+   
+    // printHello();
+  cout<<"value="<<value<<endl;
+  
+    return 0; 
+
+}
