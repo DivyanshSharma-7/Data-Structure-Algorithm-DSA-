8085ff327d058a526ee6338e3d58090b8b88b851
Divyansh Sharma <ds3741777@gmail.com>
2026-07-10 15:41:17 +0530

    Add file

---
+++ 2D Arrays/Basics.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std ;
+ int main(){
+
+    int arr[5]={3,5,7,2,6};
+    int arr1[3][2]={{2,3},{3,6},{3,7}};
+int rows =3;
+int cols=2;
+ arr1[2][1]=59;
+
+    cout<<arr[3]<<endl;
+    cout<<arr1[2][1]<<endl;
+    return 0;
+ }
